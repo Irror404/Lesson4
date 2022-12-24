7e77b69a8210ded58cfa88c8589d5fbb66d1ae6a
Irina <irina.m.protasevich@gmail.com>
2022-12-24 17:37:01 +0400

    feat: add solutions to Tasks of 'Looping statements'

--- src/lesson04/Task01.java
+++ src/lesson04/Task01.java
@@ -2,10 +2,8 @@ package lesson04;
 
 public class Task01 {
     public static void main(String[] args){
-        int j = 0;
-        while (j <= 10) {
-            System.out.println(j);
-            j++;
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
         }
     }
 }
--- src/lesson04/Task02.java
+++ src/lesson04/Task02.java
@@ -2,11 +2,8 @@ package lesson04;
 
 public class Task02 {
     public static void main(String[] args){
-        int i = 10;
-        while(i >= 0) {
+        for (int i = 10; i >= 0; --i) {
             System.out.println(i);
-            i--;
         }
     }
-
 }
